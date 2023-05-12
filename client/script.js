e553210f3f9762e217130db4982b4da4361c6b45
Sheikh-Sanuar <100768383+Sheikh-Sanuar@users.noreply.github.com>
2023-05-12 15:02:03 +0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -94,10 +94,8 @@ const handleSubmit = async (e) => {
 
 		typeText(messageDiv, parsedData);
 	} else {
-		const err = await response.text();
-
+		
 		messageDiv.innerHTML = 'Something went wrong';
-		alert(err);
 	}
 };
 
